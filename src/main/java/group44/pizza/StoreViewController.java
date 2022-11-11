a78fac620ea6e6b1594aac12f59687c856e4b992
Dhruv Samdani <dsamdani@asu.edu>
2022-11-10 23:46:27 -0700

    Updated storeview and started adding functionality for displaying orders

--- src/main/java/group44/pizza/StoreViewController.java
+++ src/main/java/group44/pizza/StoreViewController.java
@@ -1,16 +1,43 @@
 package group44.pizza;
 
+import group44.pizza.storage.Order;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class StoreViewController{
+import group44.pizza.storage.Database;
+
+public class StoreViewController implements Initializable {
+    @FXML
+    public ListView<Order> orders;
+    @FXML
+    private Label asuid;
+    @FXML
+    private Label toppings;
+    @FXML
+    private Label pizzaType;
+
+    String currentOrder;
+
+
     @FXML
     public void storeViewStart(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("StoreView.fxml"));
@@ -23,5 +50,42 @@ public class StoreViewController{
         stage.setX(x);
         stage.setY(y);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ArrayList<String> toppings_ = new ArrayList<String>();
+        toppings_.add("Mushrooms");
+        toppings_.add("Pineapple");
+        Order order1 = new Order("122222", "cheese", toppings_, "12:00");
+        Order order2 = new Order("1222922", "pepperoni", toppings_, "12:00");
+        Order order3 = new Order("12222", "cheese", toppings_, "12:00");
+        orders.getItems().add(order1);
+        orders.getItems().add(order2);
+        orders.getItems().add(order3);
+        orders.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Order>(){
+            @Override
+            public void changed(ObservableValue<? extends Order> observableValue, Order order, Order t1) {
+                System.out.println(t1);
+                asuid.setText(t1.getAsuID());
+                pizzaType.setText(t1.getPizzaType());
+                toppings.setText(String.join("\n", t1.getToppings()));
+
+
+            }
+        });
+
+        orders.setCellFactory(orderListView -> new ListCell<Order>() {
+            @Override
+            protected void updateItem(Order order, boolean empty) {
+                super.updateItem(order, empty);
+                if (empty) {
+                    String text = "test";
+                    setText(null);
+                } else {
+                    setText("Order: " + String.valueOf(order.getId() + 1));
+                }
+            }
+        } );
     }
 }
